e327014d0023ccd1389af102c603a636dc993be2
lam4est <lam058b@gmail.com>
2024-10-24 17:07:40 +0700

    create components, pages, and base navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Doctors from './pages/Doctors'
+import Login from './pages/Login'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import MyProfile from './pages/MyProfile'
+import Appointment from './pages/Appointment'
+import MyAppointments from './pages/MyAppointments'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <div className='text-red-600'>App</div>
+    <div className='mx-4 sm:mx-[10%]'>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/doctors' element={<Doctors/>} />
+        <Route path='/doctors/:speciality' element={<Doctors/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/contact' element={<Contact/>} />
+        <Route path='/my-profile' element={<MyProfile/>} />
+        <Route path='/appointment/:docId' element={<Appointment/>} />
+        <Route path='/my-appointment' element={<MyAppointments/>} />
+      </Routes>
+
+    </div>
   )
 }
 
