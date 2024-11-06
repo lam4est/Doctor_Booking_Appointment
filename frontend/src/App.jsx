f6f79490016437d5d7d6479ae0b379bac405024d
lam4est <lam058b@gmail.com>
2024-11-07 02:57:11 +0700

    create my-appointment page for patients

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
         <Route path='/contact' element={<Contact/>} />
         <Route path='/my-profile' element={<MyProfile/>} />
         <Route path='/appointment/:docId' element={<Appointment/>} />
-        <Route path='/my-appointment' element={<MyAppointments/>} />
+        <Route path='/my-appointments' element={<MyAppointments/>} />
       </Routes>
       <Footer/>
     </div>
