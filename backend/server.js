4ee6fe5330c2fb7cedbff6d0bfd2a474bebe4f16
lam4est <lam058b@gmail.com>
2024-11-12 23:35:15 +0700

    create API to add doctor data in database. Fix bug in models folder

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import 'dotenv/config' 
 import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
+import adminRouter from './routes/adminRoute.js'
 
 // app config
 const app = express()
@@ -11,10 +12,12 @@ connectDB()
 connectCloudinary()
 
 // middlewares
-app.use(express.json())
-app.use(cors())
+app.use(cors());
+app.use(express.json()); 
 
 // api endpoints
+app.use('/api/admin', adminRouter);
+
 app.get('/',(req,res) => {
     res.send('API WORKING')
 })
