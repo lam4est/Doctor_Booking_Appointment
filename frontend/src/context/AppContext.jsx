45607f0ea93e6e5debd5f0817f1a941d721aa751
lam4est <lam058b@gmail.com>
2024-11-22 23:20:04 +0700

    display all doctor list on frontend

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,16 +1,40 @@
 import { createContext } from "react";
 import { doctors } from "../assets/assets";
+import axios from 'axios'
+import { useState } from "react";
+import { useEffect } from "react";
+import { toast } from "react-toastify";
 
 export const AppContext = createContext()
 
 const AppContextProvider = (props) => {
 
     const currencySymbol = '$'
+    const backendUrl = import.meta.env.VITE_BACKEND_URL
+    const[doctors,setDoctors] = useState([])
 
     const value = {
         doctors,
         currencySymbol
     }
+
+    const getAllDoctorsData = async () =>{
+        try {
+            const {data} = await axios.get(backendUrl + '/api/doctor/list')
+            if(data.success){
+                setDoctors(data.doctors)
+            } else {
+                toast.error(data.message)
+            }
+        } catch (error) {
+            console.log(error)
+            toast.error(error.message)
+        }
+    }
+
+    useEffect(()=>{
+        getAllDoctorsData()
+    })
 
     return(
         <AppContext.Provider value={value}>
